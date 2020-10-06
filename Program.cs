db6657af656ed0d156b0d3d5f38ed45939b01532
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-06 12:51:35 +0300

    Написано привітання зі свІтом

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISMFirstSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
